96c9768cd97ee38dff0ad6d356a2299597aa08aa
Rono Beatrice <ronobc24@gmail.com>
2023-10-13 17:31:03 +0300

    Enhanced app.js with global variable usage

---
+++ app.js
@@ -0,0 +1,10 @@
+// __dirname Global Variable
+console.log(__dirname); ///home/rono/Documents/100 Days of code/NodeJSTut
+
+// __filename Global Variable
+console.log(__filename); ///home/rono/Documents/100 Days of code/NodeJSTut/app.js
+
+global.myVariable = "Hello World!"; //Defining the global variable in NOdeJS
+console.log(myVariable); //Accessing the global variable
+
+
